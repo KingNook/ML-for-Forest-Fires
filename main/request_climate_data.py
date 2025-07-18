89f947c2397447c9c66dd6f352735a5a44a5c524
Michael <mike04.lin@gmail.com>
2025-07-18 20:24:51 +0100

    feat: add function to send CDS request

--- main/request_climate_data.py
+++ main/request_climate_data.py
@@ -1,6 +1,11 @@
+import numpy as np
 import cdsapi
+import datetime
 
-import CDS_api_requests
+import time
+
+import CDS_api_requests, unzippify
+from extents import ALASKA_RANGE_EXTENT
 
 # need whole dataset at once for calculations -- will be calculating running average for these (nb will also need 180 days prior to start of 2010 to calculate initial averages)
 # write request specifically for these separately
@@ -38,8 +43,46 @@ soil_moisture_variables = [
 
 all_vars = proxy_base_variables + wind_speed_variables + standard_variables
 
-requests = CDS_api_requests.default_era5_request(standard_variables)
-dataset = 'reanalysis-era5-land'
-
 client = cdsapi.Client()
+
+def track_runtime(func):
+
+    def tracked_fn(*args, **kwargs):
+        start_time = time.time()
+        out = func(*args, **kwargs)
+        run_time = time.time() - start_time
+
+        print(f'[{func.__name__()}] run time: {run_time}s')
+
+        return out
+    
+    return tracked_fn
+
+def parse_date(date: datetime.date) -> str:
+    '''
+    takes a `date` object and returns the string `yyyy-mm-dd`
+    '''
+
+    return f'{date.year}-{date.month}-{date.day}'
+
+@track_runtime
+def send_request(
+    request: CDS_api_requests.Request, 
+    dataset: str ='reanalysis-era5-land', 
+    target: str = '', # lands in 
+    auto_unzip: bool = False,
+    auto_remove: bool = True,
+    client=client
+    ) -> None:
+    '''
+    idk what would be sensible to return tbh
+
+    if auto_unzip, then will unzip the file imeediately once downloaded // need to implement this
+    '''
+
+    if target == '':
+        client.retrieve(dataset, request).download()
+    else:
+        client.retrieve(dataset, request, target)
 client.retrieve(dataset, requests[0]).download()
+    return None
