0e38c6dbae9a0abd3020d54f283572179f460e53
Michael <mike04.lin@gmail.com>
2025-07-18 20:22:46 +0100

    boilerplate model code

--- main/model.py
+++ main/model.py
@@ -1,8 +1,14 @@
 from sklearn.ensemble import RandomForestClassifier
+import pickle
+import numpy as np
 
-x_data = []
-y_data = []
+x_data = np.array([1, 2, 4]).reshape(-1, 1)
+y_data = np.array([1, 4, 16])
 
 rf = RandomForestClassifier()
 
 rf.fit(x_data, y_data)
+
+with open('model_file.txt', 'wb') as model_file:
+
+    pickle.dump(rf, file = model_file)
