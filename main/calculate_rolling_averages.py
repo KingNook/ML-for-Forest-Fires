ea43c424d6cf96da3e0988ed03173e088ec86ba7
Michael <mike04.lin@gmail.com>
2025-07-23 11:30:17 +0100

    chore: updated to match new geodataclasses

--- main/calculate_rolling_averages.py
+++ main/calculate_rolling_averages.py
@@ -4,18 +4,21 @@ calculate the 30 day rolling averages, then functions to read off 90, 180 day av
 
 import open_data
 
-from geodataclass import MonthlyData, Flattened_MonthlyData
+from geodataclass import HourlyData
 from request_climate_data import track_runtime
 
 DATA_PATH = './data/alaska_prior'
 
-data = Flattened_MonthlyData(open_data.open_data_dir(DATA_PATH))
+data = HourlyData(open_data.open_data_dir(DATA_PATH))
 
 @track_runtime
-def tot_data(data, n):
+def tot_data(data, n=-1):
     '''
     running 30d totals
     '''
+
+    if n < 0:
+        n = len(data) + n + 1
 
     prev_total = sum(data[:30])
     running_totals = []
@@ -29,6 +32,3 @@ def tot_data(data, n):
         running_totals.append(new_total)       
 
     return running_totals
-
-if __name__ == '__main__':
-    print(tot_data(data, len(data))[0])
