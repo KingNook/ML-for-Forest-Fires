d6963e4a0d330527f326add079afd965579f12c5
Michael <mike04.lin@gmail.com>
2025-07-31 15:45:48 +0100

    feat: new class FlattenedTruthTable
    takes in a pandas dataframe and returns 1 or 0 depending on whether there exists a datapoint there; used for fire data

---
+++ main/open_fire_data.py
@@ -0,0 +1,62 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+    fire_data = pd.read_csv(
+        './data/FIRE/alaska_range_csv/data.csv',
+        parse_dates = ['acq_date']
+    )
+
+    veg_files = fire_data[
+        (fire_data['type'] == 0) & (fire_data['confidence'] >= 50)
+    ]
+
+    important_vars = [
+        'latitude',
+        'longitude',
+        'acq_date',
+        'acq_time'
+    ]
+
+    var_series = [
+        veg_files[var] for var in important_vars
+    ]
+
+    var_series[0] = var_series[0].round(1)
+    var_series[1] = var_series[1].round(1)
+    var_series[2] = var_series[2]
+    var_series[3] = (var_series[3]/100).apply(np.ceil).astype(np.float64)
+
+    fire_ds = pd.concat(
+        objs = var_series,
+        axis = 1,
+        names = important_vars
+    )
+
+class FlattenedTruthTable:
+    '''
+    holds a dataframe and a coord range -- translates index into long/lat/time coords, then checks against table to see if there is an entry
+    '''
+
+    def __init__(self, data):
+        self.data = data
+        self.grid_shape = data.shape
+
+    def __getitem__(self, key):
+
+        grid_cells = int(np.prod(self.grid_shape))
+        
+        ## time/space split -- quotient is hour index, remainder is corr cell
+        ts_split = divmod(key, grid_cells) 
+        long, lat = divmod(ts_split[1], self.grid_shape[1])
+
+        ## return 1 if yes fire and 0 if not
+        fire_series = self.data[
+            (self.data['longitude']==long) & (self.data['latitude']==lat) & (self.data['acq_time']==ts_split[0]) & (self.data['confidence'] >= 70)
+        ]
+
+        if fire_series.empty:
+            return 0
+        else:
+            return 1
