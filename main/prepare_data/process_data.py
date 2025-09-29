55422f888f703de5668efdc312a182f2d676f00a
Michael <mike04.lin@gmail.com>
2025-09-29 11:21:23 +0100

    feat: function to add fire to dataset

--- main/prepare_data/process_data.py
+++ main/prepare_data/process_data.py
@@ -7,17 +7,22 @@ Contains code to
 - convert data from GRIB -> Zarr
 - calculates wind speed
 - calculate proxy variables + write to new Zarr
+- convert a DataFrame to a Dataset
 - add fire data to the dataset
 
 Note that code here only calculates the following proxy variables (as of 28/08/25):
 - 30/90/180 day rolling averages for Temperature
 - 30/90/180 day rolling averages for Precipitation
 '''
+from constants import DEFAULT_FIRE_CONFIG
 
 import xarray as xr
 from xarray import Dataset, DataArray
+
 import pandas as pd
 from pandas import DataFrame
+
+import numpy as np
 
 import os
 
@@ -98,9 +103,128 @@ def compute_proxy(main_ds: Dataset, prior_ds: Dataset, proxy_var: str, timeframe
 
     return unstacked
 
-def add_fire_data(main_dataset: Dataset, fire_data: DataFrame, config: dict):
+
+def process_fire_data(df: DataFrame):
     '''
-    Adds fire data to a dataset -- note that fire data was downloaded from MODIS as a csv, so it is easiest to read as a DataFrame using pandas.read_csv
+    Processes fire data from MODIS to a useable format
+
+    Parameters
+    ----------
+    df: DataFrame
+        Dataframe with fire data read from the csv provided by MODIS
+
+    Returns
+    -------
+    clean_df: DataFrame
+        Dataframe containing fire data in a compatible form
+    '''
+
+    df = df.copy()
+
+    df['latitude'] = df['latitude'].round(1)
+    df['longitude'] = df['longitude'].round(1)
+
+    df['acq_time'] = np.floor(df['acq_time']/100).astype(np.float64)
+    
+    mask = df['acq_time'] == 0
+
+    df.loc[mask, 'acq_time'] = 24.0
+    df.loc[mask, 'acq_date'] = df.loc[mask, 'acq_date'] - pd.Timedelta(days=1)
+
+    return df
+
+def da_from_df(df: DataFrame, ds: Dataset, dims: list = ['latitude', 'longitude', 'time', 'step']) -> DataArray:
+    '''
+    Turns the fire DataFrame into a DataArray, ready to add to the main Dataset
+
+    Parameters
+    ----------
+    df: DataFrame
+        DataFrame containing Fire data -- read directly from csv (will be processed)
+
+    ds: Dataset
+        Reference Dataset to get reference dimensions
+
+    dims: list (optional)
+        Choice of dimensions to put into the DataArray. By default, this will be 'latitude', 'longitude', 'time' and 'step'
+
+    Returns
+    -------
+    fire_array: DataArray
+        DataArray containing fire data, with dimensions matching the input data
+    '''
+
+    ## we get
+    # dims = names
+    # dim_vals = values
+    # reference_dims = dict
+
+    reference_dims = {
+        ref_dim : ds[ref_dim].data for ref_dim in dims
+    }
+
+    dim_vals = list(reference_dims.values())
+
+
+    df_proc= process_fire_data(df)[['longitude', 'latitude', 'acq_date', 'acq_time']].drop_duplicates()
+
+    df_renamed = df_proc.rename(columns = {
+        'acq_date': 'time',
+        'acq_time': 'step'
+    })
+
+    df_renamed['presence'] = 1
+
+    da = df_renamed.set_index(dims)
+
+    idx = pd.MultiIndex.from_product(dim_vals, names = dims)
+    da = da.reindex(idx)
+    da = da['presence'].to_xarray()
+
+    da = da.reindex(reference_dims).fillna(0)
+
+    return da
+
+    ds = xr.Dataset()
+    ds['fire'] = da
+
+    ds = ds.assign(time=lambda ds: ds.time.astype('datetime64[ns]')).transpose('latitude', 'longitude', 'time', 'step')
+
+    return ds
+
+def add_DAILY_fire_data(main_dataset: Dataset, fire_data: DataFrame, config: dict = DEFAULT_FIRE_CONFIG):
+    '''
+    NOTE: need to change helper functions (`process_fire_data` and `da_from_df`) to take configs before implementing this
+    
+    Adds daily fire data to a dataset; note that this considers fires from 1:00 to 1:00 as opposed to 0:00 to 24:00 as may be expected;
+    this doesn't really have a huge effect since there are not a lot of fires that happen entirely between 0:00 and 1:00 anyways
+
+    Parameters
+    ----------
+    main_dataset: Dataset
+        Dataset of input data (would like to add fire data to this)
+
+    fire_data: DataFrame
+        DataFrame (as read from csv) of fire data -- should be sourced from MODIS (other formats not currenlty supported)
+
+    config: dict (optional)
+        Thresholds for what is considered a fire; by default this is
+            - confidence > 70
+            - type == 0 (ie vegetation fire)
+    
+    Returns
+    -------
+    new_dataset: Dataset
+        Dataset with new 'fire' variable
+    '''
+
+    raise NotImplementedError
+
+def add_HOURLY_fire_data(main_dataset: Dataset, fire_data: DataFrame, config: dict = DEFAULT_FIRE_CONFIG):
+    '''
+    NOTE: need to check overpass frequency etc to see if it's even worth doing hourly data
+    
+    Adds hourly fire data to a dataset -- note that fire data was downloaded from MODIS as a csv, so it is easiest to read as a DataFrame using pandas.read_csv
     
     It may be worth looking at the [MODIS user guide](https://modis-fire.umd.edu/files/MODIS_C6_C6.1_Fire_User_Guide_1.0.pdf) (see page 39/64 for information about column headers)
     
@@ -120,10 +244,22 @@ def add_fire_data(main_dataset: Dataset, fire_data: DataFrame, config: dict):
     Returns
     -------
     new_dataset: Dataset
-        Dataset with new 'fire' column
+        Dataset with new 'fire' variable
     '''
 
-    raise NotImplementedError
+    params = config.keys()
+
+    fire_ds = fire_data
+
+    if 'confidence' in params:
+        fire_ds = fire_ds[fire_ds['confidence'] > config['confidence']]
+
+    if 'type' in params:
+        fire_ds = fire_ds[fire_ds['type'].isin(config['type'])]
+
+    fda = da_from_df(fire_data)
+
+    return main_dataset.assign(fire = fda)
 
 def setup_dataset(main_path: str, prior_path: str, proxy_config: dict, drop_vars: bool = False) -> Dataset:
     '''
